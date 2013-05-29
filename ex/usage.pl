a481f2f208aeacd0c87a10cbccb6016cc10602dc
Mons Anderson <mons@cpan.org>
2013-05-29 17:47:44 +0400

    modify example

--- ex/usage.pl
+++ ex/usage.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/env perl
 
+# for nodetach mode run me with -f
+# -x1 denotes only one exception in child before complete exit
+# 	perl usage.pl -f start -x1
+
 use lib::abs '../lib';
 use Daemond::Lite;
 
@@ -7,27 +11,34 @@ name 'sample';
 config 'daemon.conf';
 children 1;
 pid '%n.%u.pid';
-nocli;
-syslog 'local0';
+#nocli; # to disable start/stop/restart commands
 
-sub start {
+sub check { # runned before fork and detach
+	my $self = shift;
+	$self->{this} = bless{},'main';
+	warn "checking for $self->{cfg}{additional}{section}";
+	die "Need configuration" unless $self->{cfg}{additional}{section};
+}
+
+sub start { # runned after detach but before child fork
 	warn "$$ starting";
 }
 
-sub run {
+sub run { # runned after fork in every child process
 	warn "$$ run";
 	my $self = shift;
-	$self->{run} = 1;
-	while($self->{run}) {
+	$self->{this}{run} = 1;
+	while($self->{this}{run}) {
+		warn "working...";
 		sleep 1;
 	}
 	#die "$$ gone $!";
 }
 
-sub stop {
+sub stop { # caloled in every child when signaled
 	warn "$$ stop";
 	my $self = shift;
-	$self->{run} = 0;
+	$self->{this}{run} = 0;
 }
 
 runit();
